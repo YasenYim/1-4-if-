208e541b94d57d3a4bf3d0a11edeac33c80d8692
niko <66620705+nikoLua@users.noreply.github.com>
2022-11-08 12:39:53 +0800

    Add files via upload

---
+++ 1-4 if判断/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace _1_4_if判断
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int a = 3;
+
+            //// string转化为Int类型，如果不转化的话，下面无法进行大小的比较
+            //string s = Console.ReadLine();
+            //a = int.Parse(s);
+
+
+
+
+            //if (a < 0)
+            //{ Console.WriteLine("a < 0"); }
+            //else if (a > 0)
+            //{ Console.WriteLine("a是正数"); }
+            //else
+            //{ Console.WriteLine("a是0"); }
+
+            //Console.WriteLine("结束");
+
+            //// 练习1
+            ///* 1、输入一个数字，如果是2,3或5的倍数，则打印0,；否则打印它本身。
+
+            //string s = Console.ReadLine();
+            //int a = int.Parse(s);
+
+            //if(a % 2 == 0 || a % 3 == 0 || a % 5 == 0 )
+            //{ Console.WriteLine(0); }
+
+            //else
+            //{ Console.WriteLine(a); }
+
+
+           
+            // 练习1.2
+            // * 输入一个数字，如果是2的倍数，则打印它乘以2的结果；如果是3或5的倍数则类似2的倍数，也打印3或5的结果；否则打印0
+            // */
+            //string s = Console.ReadLine();
+            //int a = int.Parse(s);
+
+            //if(a % 2 == 0)
+            //{ Console.WriteLine(a * 2); }
+
+            //else if(a % 3 == 0)
+            //{ Console.WriteLine(a * 3); }
+
+            //else if(a % 5 == 0)
+            //{ Console.WriteLine(a * 5); }
+
+            //else 
+            //{ Console.WriteLine(a); }
+
+
+           
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
